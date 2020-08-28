8669f33ff1fea63be7898308c1c4f2def08d040b
Matrix53 <1079207272@qq.com>
2020-08-28 12:43:36 +0800

    update the Chess Class

--- Chess.py
+++ Chess.py
@@ -1,3 +1,60 @@
-class chess:
+import pygame
+
+'''
+Class Chess is used to represent the game, and here're some settings.
+
+postion:
+    A Tuple. Represent the position of a piece, ans pos[0] is the abscissa.
+    The origin is on the left top, and the x-axis is horizontal.
+
+player:
+    An integer. 1 means the black side, 2 means the white side, 0 means nothing.
+    The black side always drops pieces first.
+
+board:
+    A 2D list of int. board[x][y] means which side the piece in (x,y) is belonged to.
+
+pieceStack:
+    A stack of pos. The stack is used to record the game and implement undoing a drop.
+
+playerName:
+    The name of a player. Only used in PVP mode.
+
+'''
+class Chess:
+    #initialize the board
     def __init__(self):
-        
+        self.board=[[0 for i in range(0,15)] for j in range(0,15)]
+        self.pieceStack=[]
+
+    def drawBoard(self):
+        pass
+
+    def drawPiece(self,pos):
+        pass
+    
+    def drawAllPieces(self):
+        pass
+
+    def drawPlayer(self,player,playerName=''):
+        pass
+
+    #fill background with special brown
+    def drawBackground(self,screen):
+        screen.fill((255,128,64))
+    
+    def isWinner(self,pos):
+        pass
+
+    def dropPiece(self,pos,player):
+        pass
+
+    def undoDrop(self):
+        pass
+
+#The function is used to test the Chess Class
+def unitTest():
+    pass
+
+if __name__=='__main__':
+    unitTest()
